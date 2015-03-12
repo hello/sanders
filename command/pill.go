c6e28514cb638a34447acef621af6683932822f8
Jackson Chen <jchen@Jacksons-MacBook-Pro.local>
2015-03-12 14:14:26 -0700

    fix copypasta error

--- command/pill.go
+++ command/pill.go
@@ -8,19 +8,19 @@ import (
 	"strings"
 )
 
-type BuildCommand struct {
+type PillCommand struct {
 	Ui cli.ColoredUi
 }
 
-func (c *BuildCommand) Help() string {
+func (c *PillCommand) Help() string {
 	helpText := `Usage: hello pill $key|$csv`
 	return strings.TrimSpace(helpText)
 }
 
-func (c *BuildCommand) Run(args []string) int {
+func (c *PillCommand) Run(args []string) int {
 	return 0
 }
 
-func (c *BuildCommand) Synopsis() string {
+func (c *PillCommand) Synopsis() string {
 	return "Upload pill key and csv to Hello HQ"
 }
