5cd4ac00f47e985a3dd4b3c0146d0a90c97a7486
Jackson Chen <jchen@Jacksons-MacBook-Pro.local>
2015-03-12 14:52:44 -0700

    more stub code to handle files

--- command/pill.go
+++ command/pill.go
@@ -1,9 +1,7 @@
 package command
 
 import (
-	/*
-	 *"fmt"
-	 */
+	"fmt"
 	/*
 	 *"github.com/crowdmob/goamz/aws"
 	 */
@@ -24,9 +22,20 @@ func (c *PillCommand) Help() string {
 }
 
 func (c *PillCommand) Run(args []string) int {
+	if 0 == len(args) {
+		c.Ui.Error(fmt.Sprintf("Please provide a file name."))
+	} else {
+		for _, fname := range args {
+			upload(c, fname)
+		}
+	}
 	return 0
 }
 
 func (c *PillCommand) Synopsis() string {
 	return "Upload pill key and csv to Hello HQ"
+}
+
+func upload(c *PillCommand, fname string) {
+	c.Ui.Info(fmt.Sprintf("Uploading file %s", fname))
 }
