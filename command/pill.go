531fabf4caf6bd01a03ba857645af2192d2f5dbe
Jackson Chen <jchen@sayhello.com>
2015-03-26 11:43:42 -0700

    print out partial key for debuggin

--- command/pill.go
+++ command/pill.go
@@ -24,7 +24,7 @@ func (c *PillCommand) Help() string {
 }
 
 func (c *PillCommand) Run(args []string) int {
-	c.Ui.Info(fmt.Sprintf("Establishing connection with %s", string(AwsSecretKey)[:2]))
+	c.Ui.Info(fmt.Sprintf("Establishing connection with key %s", string(AwsSecretKey)[:2]))
 	bucket := connectToS3(AwsAccessKey, AwsSecretKey)
 
 	if 0 == len(args) {
