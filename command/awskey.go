29c87faf261edd87ef46660ee04b44a231c1231c
Jackson Chen <jchen@sayhello.com>
2015-06-16 10:44:45 -0700

    show jabil/pch keys

---
+++ command/awskey.go
@@ -0,0 +1,17 @@
+package command
+
+const (
+	AwsAccessKey = "AKIAJOEMH4WUJK77TDGQ"
+	AwsSecretKey = "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"
+)
+
+/*
+* jabil
+*AwsAccessKey = "AKIAJOEMH4WUJK77TDGQ"
+*AwsSecretKey = "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"
+ */
+/*
+* pch
+*AwsAccessKey = "AKIAI5ISENMNOIXWMLVA"
+*AwsSecretKey = "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"
+ */
