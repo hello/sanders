1c7cbff1234951723692f424b100d7592eb95d77
jakepic1 <jake@sayhello.com>
2016-02-18 13:23:39 -0800

    Fix version grabbing

--- command/status.go
+++ command/status.go
@@ -94,7 +94,7 @@ func (c *StatusCommand) Run(args []string) int {
 			imageVersion := ""
 			if lcNames[*state.InstanceId] != "" {
 				parts = strings.SplitAfterN(lcNames[*state.InstanceId], "-", 4)
-				imageVersion = parts[3]
+				imageVersion = parts[len(parts)-1]
 			} else {
 				parts = strings.SplitAfterN(amiName, "-", 4)
 				imageVersion = parts[2]
